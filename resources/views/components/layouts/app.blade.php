7fdaa8ba0cd5e9df8aa463c2a702042659192b81
D4V1D-101 <115972590+D4V1D-101@users.noreply.github.com>
2025-03-31 12:57:48 +0200

    asda

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -34,7 +34,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav m-auto mb-2 mb-lg-0">
                     <li class="nav-item"> <a wire:navigate class="nav-link" href="{{route('home')}}">Home</a></li>
-                    <li class="nav-item "> <a wire:navigate class="nav-link" href="{{route('page',2)}}">About Us</a></li>
+                    <li class="nav-item "> <a wire:navigate class="nav-link" href="{{route('page',1)}}">About Us</a></li>
                     <li class="nav-item "> <a wire:navigate class="nav-link" href="{{route('servicesPage')}}">Games</a></li>
                     <li class="nav-item "> <a wire:navigate class="nav-link" href="{{ route('teamPage') }}">Our Team</a></li>
                     <li class="nav-item "><a wire:navigate class="nav-link " href="{{ route('blog') }}">News</a></li>
@@ -72,24 +72,22 @@
         <div class="row justify-content-between">
             <div class="col-lg-2 col-md-4 col-6 mb-4">
                 <div class="footer-widget">
-                    <h5 class="mb-4 text-primary font-secondary">Service</h5>
+                    <h5 class="mb-4 text-primary font-secondary">Quick Links</h5>
                     <ul class="list-unstyled">
-                        <li class="mb-2"><a href="service-details.html">Digital Marketing</a></li>
-                        <li class="mb-2"><a href="service-details.html">Web Design</a></li>
-                        <li class="mb-2"><a href="service-details.html">Logo Design</a></li>
-                        <li class="mb-2"><a href="service-details.html">Design</a></li>
-                        <li class="mb-2"><a href="service-details.html">SEO</a></li>
+                        <li class="mb-2"><a wire:navigate class="nav-link" href="{{route('home')}}">Home</a></li>
+                        <li class="mb-2"><a href="{{route('page',1)}}" wire:navigate>About Us</a></li>
+                        <li class="mb-2"><a wire:navigate class="nav-link" href="{{route('servicesPage')}}">Games</a></li>
+
                     </ul>
                 </div>
             </div>
             <div class="col-lg-2 col-md-4 col-6 mb-4">
                 <div class="footer-widget">
-                    <h5 class="mb-4 text-primary font-secondary">Quick Links</h5>
+                    <h5 class="mb-4 text-primary font-secondary"></h5>
                     <ul class="list-unstyled">
-                        <li class="mb-2"><a href="{{route('page',1)}}" wire:navigate>About Us</a></li>
-                        <li class="mb-2"><a href="#!">Contact Us</a></li>
-                        <li class="mb-2"><a href="#!">Blog</a></li>
-                        <li class="mb-2"><a href="#!">Team</a></li>
+                        <li class="mb-2"> <a wire:navigate class="nav-link" href="{{ route('teamPage') }}">Our Team</a></li>
+                        <li class="mb-2"><a wire:navigate class="nav-link " href="{{ route('blog') }}">News</a></li>
+                        <li class="mb-2"><a wire:navigate class="nav-link " href="{{ route('faqs') }}">Faq</a></li>
                     </ul>
                 </div>
             </div>
@@ -98,7 +96,7 @@
                     <h5 class="mb-4 text-primary font-secondary">Other Links</h5>
                     <ul class="list-unstyled">
                         <li class="list-inline-item me-4"><a class="text-black" href="{{route('page',2)}}" wire:navigate>Privacy Policy</a></li>
-                        <li class="list-inline-item me-4"><a class="text-black" href="{{route('page',1)}}" wire:navigate>Terms &amp; Conditions</a></li>
+                        <li class="list-inline-item me-4"><a class="text-black" href="{{route('page',3)}}" wire:navigate>Terms &amp; Conditions</a></li>
                     </ul>
                 </div>
             </div>
