5d6e9fcf07c435ea833e9324267e312b4736c2b2
D4V1D-101 <115972590+D4V1D-101@users.noreply.github.com>
2025-03-31 23:10:04 +0200

    vegpont

--- app/Http/Controllers/Api/GameController.php
+++ app/Http/Controllers/Api/GameController.php
@@ -1,10 +1,6 @@
 <?php
-// php artisan make:controller Api/GameController
-
-// In app/Http/Controllers/Api/GameController.php
 namespace App\Http\Controllers\Api;
 
-use App\Models\Game;
 use App\Models\Games;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -13,18 +9,35 @@ class GameController extends Controller
 {
     public function index()
     {
-        $games = Games::with(['genres', 'developer', 'publisher'])->get();
-        dump($games); 
+        $games = Games::select([
+            'name',
+            'short_desc',
+            'exe_name',
+            'description',
+            'image_path',
+            'release_date',
+            'download_link',
+            'developer_id',
+            'publisher_id'
+        ])->get();
+
         return response()->json($games);
     }
 
     public function show($id)
     {
-        $game = Games::with(['genres', 'developer', 'publisher', 'articles'])->findOrFail($id);
-        dump($game);
+        $game = Games::select([
+            'name',
+            'short_desc',
+            'exe_name',
+            'description',
+            'image_path',
+            'release_date',
+            'download_link',
+            'developer_id',
+            'publisher_id'
+        ])->findOrFail($id);
+
         return response()->json($game);
     }
 }
-
-
-
